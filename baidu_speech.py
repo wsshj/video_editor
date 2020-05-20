cfa92f2ede3cdea74ecf17beb69325a390e0c976
wsshj <wsshj159@gmail.com>
2020-05-20 17:17:18 +0800

    整理

--- baidu_speech.py
+++ baidu_speech.py
@@ -1,21 +1,18 @@
 from aip import AipSpeech
- 
+
 """ 你的 APPID AK SK """
 APP_ID = '19948902'
 API_KEY = 'S4fy84Km1Ayp2zheNhaPYR53'
 SECRET_KEY = 'mqgS4MdVk2iH0V5gPyaIqxkw06ZctEKs'
- 
+
 client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
-f = open('text.txt', 'r', encoding = 'utf-8')
+f = open('testText.txt', 'r', encoding='utf-8')
 
 text = f.read()
- 
-result  = client.synthesis(text, 'zh', 1, {
-    'vol': 5,'per':0,'spd':4
-})
- 
- 
+
+result = client.synthesis(text, 'zh', 1, {'vol': 5, 'per': 0, 'spd': 5})
+
 # 识别正确返回语音二进制 错误则返回dict 参照下面错误码
 if not isinstance(result, dict):
     with open('auido.mp3', 'wb') as f:
--- editVideo.py
+++ editVideo.py
@@ -1,11 +1,29 @@
 from moviepy.editor import *
+from aip import AipSpeech
 
-video = VideoFileClip("myHolidays.mp4").subclip(50,60)
+start = 60
 
-# Make the text. Many more options are available.
-txt_clip = ( TextClip("My Holidays 2013",fontsize=70,color='white')
-             .set_position('center')
-             .set_duration(10) )
+f = open('testText.txt', 'r', encoding='utf-8')
+text = f.read()
 
-result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
-result.write_videofile("myHolidays_edited.webm",fps=25)
+""" 你的 APPID AK SK """
+APP_ID = '19948902'
+API_KEY = 'S4fy84Km1Ayp2zheNhaPYR53'
+SECRET_KEY = 'mqgS4MdVk2iH0V5gPyaIqxkw06ZctEKs'
+
+client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
+
+result = client.synthesis(text, 'zh', 1, {'vol': 5, 'per': 0, 'spd': 5})
+
+if not isinstance(result, dict):
+    with open('auido.mp3', 'wb') as f:
+        f.write(result)
+
+video = VideoFileClip("test.mp4")
+
+audio = AudioFileClip('auido.mp3')
+endtime = audio.end
+
+v = video.subclip(start, start + endtime)
+vv = v.set_audio(audio)
+vv.write_videofile("test_%s.mp4" % '', fps=25)
